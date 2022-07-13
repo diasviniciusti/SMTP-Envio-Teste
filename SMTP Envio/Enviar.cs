35aacf9ab9ef211a8a8163d76e7510df960c331d
Vinicius Dias <dias.viniciusti@gmail.com>
2022-07-13 00:56:10 -0300

    Adicionar arquivos de projeto.

---
+++ SMTP Envio/Enviar.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Mail;
+using System.Windows.Forms;
+
+namespace SMTP_Envio
+{
+	public class Enviar
+	{
+		public Enviar(string remetente, string smt, int port, string destino, string assunto, string mensagem, bool html){
+			MailMessage Email = new MailMessage();
+			Stopwatch Stop = new Stopwatch();
+			SmtpClient smtp = new SmtpClient();
+			smtp.Host = smt;
+			smtp.Port = port;
+			smtp.EnableSsl = false;
+			smtp.UseDefaultCredentials = true;
+			Email.From = new System.Net.Mail.MailAddress(remetente);
+			Email.To.Add(new System.Net.Mail.MailAddress(destino));
+			Email.Priority = MailPriority.High;
+			Email.Subject = assunto;
+			Email.SubjectEncoding = System.Text.Encoding.UTF8;
+			Email.Body = mensagem;
+			Email.BodyEncoding = System.Text.Encoding.UTF8;
+			Email.IsBodyHtml = html;
+			smtp.Send(Email);
+			MessageBox.Show("Mensagem enviada","Sucesso",MessageBoxButtons.OK, MessageBoxIcon.Information);			
+		}
+		public Enviar(string remetente, string smt, int port, string destino, string assunto, string mensagem, string anexo, bool html){
+			MailMessage Email = new MailMessage();
+			Stopwatch Stop = new Stopwatch();
+			SmtpClient smtp = new SmtpClient();
+			smtp.Host = smt;
+			smtp.Port = port;
+			smtp.EnableSsl = false;
+			smtp.UseDefaultCredentials = true;
+			Email.From = new System.Net.Mail.MailAddress(remetente);
+			Email.To.Add(new System.Net.Mail.MailAddress(destino));
+			Email.Priority = MailPriority.High;
+			Email.Subject = assunto;
+			Email.SubjectEncoding = System.Text.Encoding.UTF8;
+			Email.Body = mensagem;
+			Email.BodyEncoding = System.Text.Encoding.UTF8;
+			Email.IsBodyHtml = html;
+			Attachment arquivo = new Attachment(anexo);
+			Email.Attachments.Add(arquivo);
+			smtp.Send(Email);
+			MessageBox.Show("Mensagem enviada","Sucesso",MessageBoxButtons.OK, MessageBoxIcon.Information);			
+		}
+		public Enviar(string remetente, string smt, int port, string login, string senha, string destino, string assunto, string mensagem, bool html){
+			MailMessage Email = new MailMessage();
+			Stopwatch Stop = new Stopwatch();
+			SmtpClient smtp = new SmtpClient();
+			smtp.Host = smt;
+			smtp.Port = port;
+			smtp.EnableSsl = false;
+			smtp.UseDefaultCredentials = false;
+			smtp.Credentials = new System.Net.NetworkCredential(login,senha);
+			Email.From = new System.Net.Mail.MailAddress(remetente);
+			Email.To.Add(new System.Net.Mail.MailAddress(destino));
+			Email.Priority = MailPriority.High;
+			Email.Subject = assunto;
+			Email.SubjectEncoding = System.Text.Encoding.UTF8;
+			Email.Body = mensagem;
+			Email.BodyEncoding = System.Text.Encoding.UTF8;
+			Email.IsBodyHtml = html;
+			smtp.Send(Email);
+			MessageBox.Show("Mensagem enviada","Sucesso",MessageBoxButtons.OK, MessageBoxIcon.Exclamation);					
+		}
+		public Enviar(string remetente, string smt, int port, string login, string senha, string destino, string assunto, string mensagem, string anexo, bool html){
+			MailMessage Email = new MailMessage();
+			Stopwatch Stop = new Stopwatch();
+			SmtpClient smtp = new SmtpClient();
+			smtp.Host = smt;
+			smtp.Port = port;
+			smtp.EnableSsl = false;
+			smtp.UseDefaultCredentials = false;
+			smtp.Credentials = new System.Net.NetworkCredential(login,senha);
+			Email.From = new System.Net.Mail.MailAddress(remetente);
+			Email.To.Add(new System.Net.Mail.MailAddress(destino));
+			Email.Priority = MailPriority.High;
+			Email.Subject = assunto;
+			Email.SubjectEncoding = System.Text.Encoding.UTF8;
+			Email.Body = mensagem;
+			Email.BodyEncoding = System.Text.Encoding.UTF8;
+			Email.IsBodyHtml = html;
+			Attachment arquivo = new Attachment(anexo);
+			Email.Attachments.Add(arquivo);
+			smtp.Send(Email);
+			MessageBox.Show("Mensagem enviada","Sucesso",MessageBoxButtons.OK, MessageBoxIcon.Exclamation);					
+		}
+	}
+}
---
+++ SMTP Envio/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace SMTP_Envio
+{
+
+    public partial class Form1 : Form
+    {
+        bool req=false;
+        bool html=false;
+        
+        public Form1()
+        {
+            InitializeComponent();          
+        }
+        void CheckBox2CheckedChanged(object sender, EventArgs e)
+        {   
+            if(checkBox2.Checked){
+                textBox3.Enabled=true;
+                textBox4.Enabled=true;
+                req=true;
+            }
+            else{
+                textBox3.Enabled=false;
+                textBox4.Enabled=false;
+                req=false;
+            }
+        }
+        void Button1Click(object sender, EventArgs e)
+        {
+            string smtp=textBox1.Text;
+            int port=Convert.ToInt32(textBox2.Text);
+            string destino=textBox5.Text;
+            string assunto=textBox6.Text;
+            string mensagem=textBox7.Text;
+            string anexo=textBox8.Text;
+            string remetente=textBox9.Text;
+            if(req==true){
+                string login = textBox3.Text;
+                string senha = textBox4.Text;
+                try{
+                    if(String.IsNullOrEmpty(anexo)){
+                        new Enviar(remetente,smtp, port,login,senha,destino,assunto,mensagem,html);
+                    }
+                    else{
+                        new Enviar(remetente,smtp,port,login,senha,destino,assunto,mensagem,anexo,html);
+                    }
+                }
+                catch (Exception a){
+                    MessageBox.Show(a.Message,"Erro",MessageBoxButtons.OK,MessageBoxIcon.Error);    
+                }
+            }
+            if(req==false){
+                try{
+                    if(String.IsNullOrEmpty(anexo)){
+                        new Enviar(remetente,smtp,port,destino,assunto,mensagem,html);
+                    }
+                    else{
+                        new Enviar(remetente,smtp,port,destino,assunto,mensagem,anexo,html);
+                    }
+                }
+                catch (Exception a){
+                    MessageBox.Show(a.Message,"Erro",MessageBoxButtons.OK,MessageBoxIcon.Error);    
+                }
+            }
+        }
+        void CheckBox1CheckedChanged(object sender, EventArgs e)
+        {
+            if(checkBox2.Checked){
+                html=true;
+            }
+            else{
+                html=false;
+            }
+        }
+        void Button2Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        void Button3Click(object sender, EventArgs e)
+        {
+            OpenFileDialog way = new OpenFileDialog();
+            way.InitialDirectory = @"c:\";
+            way.RestoreDirectory = true;
+            if(way.ShowDialog()==DialogResult.OK)
+                {
+                textBox8.Text = way.FileName;
+            }
+        }
+    }
+}
